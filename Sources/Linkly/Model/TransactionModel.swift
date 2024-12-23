afce8e20d7a865b8ffcca1debabd8740d8f58e01
Nirmit Dagly <nirmitdagly@gmail.com>
2024-12-23 14:34:48 +1100

    - Modified property access level

--- Sources/Linkly/Model/TransactionModel.swift
+++ Sources/Linkly/Model/TransactionModel.swift
@@ -16,58 +16,58 @@ public struct TransactionModel: Codable {
 }
 
 public struct LinklyTransaction: Codable {
-    var txnType: String
-    var merchant: String
-    var cardType: String
-    var cardName: String
-    var rrn: String
-    var dateSettlement: String
-    var amtCash: Int
-    var amtPurchase: Int
-    var amtTip: Int
-    var authCode: Int
-    var txnRef: String
-    var pan: String
-    var dateExpiry: String
-    var track2: String?
-    var accountType: String
-    var balanceReceived: Bool
-    var availableBalance: Int
-    var clearedFundsBalance: Int?
-    var success: Bool
-    var responseCode: String
-    var responseText: String
-    var date: String
-    var catID: String
-    var caID: String
-    var stan: Int
-    var txnFlags: TransactionFlags
-    var purchaseAnalysisData: PurchaseAnalysisData?
-    var receipts: [LinklyTransactionReceipts]?
+    public var txnType: String
+    public var merchant: String
+    public var cardType: String
+    public var cardName: String
+    public var rrn: String
+    public var dateSettlement: String
+    public var amtCash: Int
+    public var amtPurchase: Int
+    public var amtTip: Int
+    public var authCode: Int
+    public var txnRef: String
+    public var pan: String
+    public var dateExpiry: String
+    public var track2: String?
+    public var accountType: String
+    public var balanceReceived: Bool
+    public var availableBalance: Int
+    public var clearedFundsBalance: Int?
+    public var success: Bool
+    public var responseCode: String
+    public var responseText: String
+    public var date: String
+    public var catID: String
+    public var caID: String
+    public var stan: Int
+    public var txnFlags: TransactionFlags
+    public var purchaseAnalysisData: PurchaseAnalysisData?
+    public var receipts: [LinklyTransactionReceipts]?
 }
 
-struct TransactionFlags: Codable {
-    var offline: String
-    var receiptPrinted: String
-    var cardEntry: String
-    var commsMethod: String
-    var currency: String
-    var payPass: String
-    var undefinedFlag6: String
-    var undefinedFlag7: String
+public struct TransactionFlags: Codable {
+    public var offline: String
+    public var receiptPrinted: String
+    public var cardEntry: String
+    public var commsMethod: String
+    public var currency: String
+    public var payPass: String
+    public var undefinedFlag6: String
+    public var undefinedFlag7: String
 }
 
-struct PurchaseAnalysisData: Codable {
-    var rfn: String?
-    var ref: String?
-    var hrc: String?
-    var hrt: String?
-    var sur: String?
-    var amt: String?
-    var cem: String?
+public struct PurchaseAnalysisData: Codable {
+    public var rfn: String?
+    public var ref: String?
+    public var hrc: String?
+    public var hrt: String?
+    public var sur: String?
+    public var amt: String?
+    public var cem: String?
 }
 
-struct LinklyTransactionReceipts: Codable {
-    var type: String
-    var receiptText: [String]
+public struct LinklyTransactionReceipts: Codable {
+    public var type: String
+    public var receiptText: [String]
 }
